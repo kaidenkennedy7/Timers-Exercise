b820b6fc13d3812452af51ae3a25cc967ddb62ac
kaidenkennedy7 <103951147+kaidenkennedy7@users.noreply.github.com>
2022-06-21 21:49:57 -0400

    Timers unit 7

---
+++ unit7Timers.js
@@ -0,0 +1,26 @@
+//Countdown
+function countDown (time) {
+	let timer = setInterval(function () {
+		time--;
+		if (time <= 0) {
+			clearInterval(timer);
+			console.log('Done!');
+		} else {
+			console.log(time);
+		}
+	}, 1000);
+}
+
+//Random Game
+function randomGame () {
+	let num;
+	let times = 0;
+	let timer = setInterval(function () {
+		num = Math.random();
+		times++;
+		if (num > 0.75) {
+			clearInterval(timer);
+			console.log('It took ' + times + ' tries.');
+		}
+	}, 1000);
+}
